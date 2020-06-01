0bc07287e4c9d8e3ec8498b90671ff8cf2ec6904
thomasCoelho <56435612+thomasCoelho@users.noreply.github.com>
2020-06-01 16:12:14 +0200

    Modif noms sources

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -1,16 +1,20 @@
-<!doctype html>
+<!DOCTYPE html>
+
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1">
         <title><?= $titre ?></title>
-		<link rel="stylesheet" type="text/css" href="contenu/css/style.css">
-		<link rel="stylesheet" type="text/css" href="contenu/css/style-admin.css">
-		<link rel="stylesheet" type="text/css" href="contenu/css/fontawesome-free-5.12.1-web/fontawesome-free-5.12.1-web/css/all.css">
-		      
+        <link rel="icon" type="image/png" href="Content/images/icone.png" />
+		<link rel="stylesheet" type="text/css" href="Content/css/style.css">
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 600px)" href="Content/css/style-s.css">
+        <link rel="stylesheet" type="text/css" media="screen and (min-width: 601px) and (max-width: 1201px)" href="Content/css/style-m.css">
+        <link rel="stylesheet" type="text/css" media="screen and (min-width: 1441px)" href="Content/css/style-xl.css">
+		<link rel="stylesheet" type="text/css" href="Content/css/fontawesome-free-5.12.1-web/fontawesome-free-5.12.1-web/css/all.css">		      
     </head>
     <body>
         <div id="global">
-            <header>
+            <header id="menu">
                 <?php require 'templates/header.php'; ?>
             </header>
             <div id="contenu">
@@ -21,8 +25,8 @@
             </footer>
         </div> <!-- #global -->
 
-        <script src="https://cdn.tiny.cloud/1/s2bmk9c8aza4wuip0f3j8kah4hvsqdh4px2h0kvesjmrkfrw/tinymce/5/tinymce.min.js" referrerpolicy="origin"/></script>  
-        <script src="js/tinymce.js"></script>
-        <script src="js/main.js"></script>
+        <script src="https://cdn.tiny.cloud/1/s2bmk9c8aza4wuip0f3j8kah4hvsqdh4px2h0kvesjmrkfrw/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>  
+        <script src="Content/js/tinymce.js"></script>
+        <script src="Content/js/main.js"></script>
     </body>
 </html>
--- Vue/vueBillet.php
+++ Vue/vueBillet.php
@@ -1,19 +1,19 @@
-<?php $this->titre = "Chapitre " .$billet['billet_id']; ?>
+<?php $this->titre = $billet['billet_title']; ?>
 
 <section id="section-billet">
 	<h1 id="title-billet"><?php echo($billet['billet_title'])?></h1>
 	<figure id="figure-billet">
-	<img class="img-billet" src="contenu/images/img-chap3.jpg">
+	<img class="img-billet" src="Content/images/img-chap.jpg" alt="Illustration chapitre">
 	</figure>
 	<?php if(isset($_COOKIE['idSession']) AND $_COOKIE['idSession'] != NULL){
-		if($_SESSION['pseudo'] == $_COOKIE['idSession']){?>	
+		if($_SESSION['pseudo'] == $_COOKIE['idSession']){ ?>	
 			<div id="div-billet-edit">
-				<a id="billet-edit" class="billet-edit">EDITER</a>
+				<a href="index.php?action=traitementAdminWrite&amp;billet=<?php echo $billet['billet_id']; ?>" id="billet-edit" class="billet-edit">EDITER</a>
 			</div>
 <?php }} ?>
 
-	<div class="text-billet"><?php echo($billet['billet_text']); ?></div>
-	<span class="date-billet"><?php echo("Publié le : " .$billet['billet_date']); ?></span>	
+	<div class="text-billet"><?php echo(strip_tags(html_entity_decode($billet['billet_text'], ENT_NOQUOTES, '<br>'))); ?></div>
+	<span class="date-billet"><?php echo("Publié le : " .$billet['date_fr']); ?></span>
 </section>
 
 <section id="section-comment">
@@ -22,34 +22,38 @@
 	<div id="div-add-comment">
 		<form id="form-add-comment" method="post" action="index.php?action=traitementComment">
 			<label class="label-add-comment" for="user-pseudo">Votre Prénom</label>
-			<input class="input-add-comment" type="text" name="user-pseudo" required>
+			<input class="input-add-comment" id="user-pseudo" type="text" name="user-pseudo" required>
 			<label class="label-add-comment" for="user-text">Votre commentaire</label>
-			<textarea class="text-add-comment" name="user-text" required></textarea>			
-			<input type="text" name="billet-id" value=<?php echo($billet['billet_id']); ?>>
+			<textarea class="text-add-comment" id="user-text" name="user-text" required></textarea>
+			<div id="hidden-input">		
+				<input type="text" name="billet-id" value=<?php echo($billet['billet_id']); ?>>
+			</div>
 			<input id="submit-add-comment" type="submit" name="user-submit" value="POSTER">
 		</form>
 	</div>
 	<?php foreach ($comments as $comment): ?>
-	<div class="div-comment">
-		<div class="div-header-comment">
-		<span class="comment-pseudo"><?php echo($comment['comment_pseudo']); ?></span>
-		<span class="comment-date">Posté le : <?php echo($comment['comment_date']); ?> </span>
-	</div>
-		<p class="comment-text"><?php echo($comment['comment_text']); ?></p>
-		<?php if(isset($_COOKIE['idSession']) AND $_COOKIE['idSession'] != NULL){
-		if($_SESSION['pseudo'] == $_COOKIE['idSession']){?>		
-		<button class="comment-edit">EDITER</button>
-		<div class="div-delete-comment">
-			<h2 class="title-delete-comment">Êtes-vous sûr de vouloir supprimer ce commentaire ?</h2>
-			<p class="text-delete-comment"><?php echo($comment['comment_text']); ?></p>
-			<div class="choice-delete-comment">
-				<a class="yes-delete-comment" href="index.php?action=Lecture&amp;id=<?php echo $comment['id_billet_comment']; ?>&amp;comment=<?php echo $comment['comment_id']; ?>">OUI</a>
-				<button class="no-delete-comment">NON</button>
+		<div class="div-comment">
+			<div class="div-content-comment">
+				<span class="comment-pseudo"><?php echo($comment['comment_pseudo']); ?></span>
+				<p class="comment-text"><?php echo($comment['comment_text']); ?></p>
+				<span class="comment-date">Posté le : <?php echo($comment['comment_date_fr']); ?> </span>
+			</div>			
+			<div class="div-comment-button">
+				<button class="button-signal-comment"><i class="fas fa-exclamation-triangle"></i><a href="index.php?action=Lecture&amp;id=<?php echo($billet['billet_id']); ?>&amp;comment_id=<?php echo($comment['comment_id']); ?>" class="a-signal-comment" > SIGNALER</a></button>
+				<?php if(isset($_COOKIE['idSession']) AND $_COOKIE['idSession'] != NULL){
+				if($_SESSION['pseudo'] == $_COOKIE['idSession']){?>		
+					<button class="comment-edit">EDITER</button>
+			</div>	
+			<div class="div-delete-comment">
+				<h2 class="title-delete-comment">Êtes-vous sûr de vouloir supprimer ce commentaire ?</h2>
+				<p class="text-delete-comment"><?php echo($comment['comment_text']); ?></p>
+				<div class="choice-delete-comment">
+					<a class="yes-delete-comment" href="index.php?action=Lecture&amp;id=<?php echo $comment['id_billet_comment']; ?>&amp;comment=<?php echo $comment['comment_id']; ?>">OUI</a>
+					<button class="no-delete-comment">NON</button>
+				</div>
 			</div>
+				<?php }
+				} ?>
 		</div>
-		<?php
-		}
-		}?>
-	</div>
 	<?php endforeach; ?>
 </section>
